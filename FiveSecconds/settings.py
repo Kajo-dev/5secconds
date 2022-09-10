194b3eda1664a8ec0d271a769b1f1abdcf686db5
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-09-10 21:45:44 +0200

    test

--- FiveSecconds/settings.py
+++ FiveSecconds/settings.py
@@ -21,6 +21,9 @@ load_dotenv('.env.local')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
@@ -129,6 +132,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "Front",
 ]
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -146,4 +150,3 @@ EMAIL_USE_TLS = True
 
 PASSWORD_RESET_TIMEOUT = 14400 #4h tokenu 
 
-#test czy działa
