e29d6043c51f27949604fb8a953483420a813b0b
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-09-11 17:25:12 +0200

    admin_register_models

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from user_log_reg.models import User
-from product_store.models import Category,Product
 
-admin.site.register(User)
-admin.site.register(Category,Product)
+
