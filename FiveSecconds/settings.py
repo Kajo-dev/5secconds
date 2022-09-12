38c43d246aebc26d381ade5725a2095cad242645
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-09-12 20:03:54 +0200

    Testuje_heroku

--- FiveSecconds/settings.py
+++ FiveSecconds/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fiveseconds-shop.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['fiveseconds-shop.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -53,12 +53,14 @@ AUTH_USER_MODEL='user_log_reg.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'FiveSecconds.urls'
@@ -127,12 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/Front/'
+STATIC_URL = '/front/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "Front",
+    BASE_DIR / "front",
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
