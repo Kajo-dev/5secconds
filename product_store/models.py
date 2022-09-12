6119d38b5c8e251d16c08fd313cd1a4a142671c3
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-09-12 23:34:27 +0200

    B-06 NewDatabase AdminSett NewModels

--- product_store/models.py
+++ product_store/models.py
@@ -26,14 +26,30 @@ class Product(models.Model):
     description = models.TextField(blank = True)
     image = models.ImageField(
         upload_to='front/img/',
-        default='front/img/default_tshirt.png'
+        default='front/img/default_tshirt.png',
         )
     price = models.DecimalField(max_digits=6, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)  
     date_created = models.DateTimeField(auto_now_add=True)
-    
+    on_sale = models.BooleanField(default=False)
+    before_sale = models.DecimalField(max_digits=6, decimal_places=2,default=0)
+    slug = models.SlugField(
+        verbose_name = ("safe URL"),
+        max_length = 255,
+    )
 
     def __str__(self):
         return self.title
 
+
+class Sizes(models.Model):
+    id_product = models.ForeignKey(
+        Product,
+        related_name='rozmiar',
+        on_delete=models.CASCADE
+    )
+    S=models.IntegerField()
+    M=models.IntegerField()
+    L=models.IntegerField()
+    XL=models.IntegerField()
