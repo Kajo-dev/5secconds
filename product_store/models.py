b3f8d013078a67795dcf4af3ffc761cfe0636d18
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-09-16 22:46:12 +0200

    B-12

--- product_store/models.py
+++ product_store/models.py
@@ -25,8 +25,8 @@ class Product(models.Model):
     title = models.CharField(max_length = 255)
     description = models.TextField(blank = True)
     image = models.ImageField(
-        upload_to='front/img/',
-        default='front/img/default_tshirt.png',
+        upload_to='product/',
+        default='front/img/default_img.png',
         )
     price = models.DecimalField(max_digits=6, decimal_places=2)
     in_stock = models.BooleanField(default=True)
