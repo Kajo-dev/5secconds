2975dbb1d0b20b5353662f6d4578980bd09a124c
Kacper <63934835+KacperFiga@users.noreply.github.com>
2022-09-20 17:44:44 +0200

    [F-03] add interactive welcoming text (#16)

--- front/js/main.js
+++ front/js/main.js
@@ -1,5 +1,11 @@
 import { Welcome } from "./Welcome.js";
 
-const welcome = new Welcome();
+const welcomeHeader = document.querySelector(".register__header__welcome");
+const welcome = new Welcome(welcomeHeader);
 
+const { hours } = welcome.getTimeOfDay();
+
+if (hours >= 17) {
+  welcomeHeader.textContent = "Dobry wieczór!";
+}
 welcome.tester();
