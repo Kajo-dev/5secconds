8498978bc840e785dc694def7280140b8d196388
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-09-20 22:47:42 +0200

    heroku

--- FiveSecconds/settings.py
+++ FiveSecconds/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATIC_URL = '/front/'
 
 STATICFILES_DIRS = [
