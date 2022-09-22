dce1988f17d50cad1236ea92e2eab1ca473394ce
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-09-22 21:17:17 +0200

    remove_stf_warn

--- FiveSecconds/settings.py
+++ FiveSecconds/settings.py
@@ -54,7 +54,6 @@ AUTH_USER_MODEL='user_log_reg.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,9 +128,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/front/'
 
