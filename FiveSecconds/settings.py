d5ddf90a0392e1aee67574c08ba8a67a8ecd1f6c
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-11-25 22:29:09 +0100

    postgres config

--- FiveSecconds/settings.py
+++ FiveSecconds/settings.py
@@ -92,15 +92,19 @@ WSGI_APPLICATION = 'FiveSecconds.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'fivesec.sqlite3',
+'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'), 
+        'USER': '36725704_fsec_testowa',
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'), 
+        'PORT': '5432',
     }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators``
 
 AUTH_PASSWORD_VALIDATORS = [
     {
