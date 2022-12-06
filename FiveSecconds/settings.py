b5ab9fe5d1835be46e8ec468ba977c6278c0c0a5
Kajo-dev <kajetandyrcz2002@gmail.com>
2022-12-06 14:20:27 +0100

    hot_fix

--- FiveSecconds/settings.py
+++ FiveSecconds/settings.py
@@ -43,10 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary',
-    'whitenoise.runserver_nostatic',    
-
+  
     #my apps
     'user_log_reg',
     'product_store',
